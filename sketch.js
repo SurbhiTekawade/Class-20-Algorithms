6e62bef101b92981e380311be6d2a0ddb3a2ceae
SurbhiTekawade <89725304+SurbhiTekawade@users.noreply.github.com>
2021-09-03 18:16:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var rect1
+var rect2
+
+
+
+
+function setup() {
+  
+  createCanvas(800,400);
+  rect1=createSprite(400, 200, 50, 50);
+  rect2 = createSprite(200,200,50,50);
+  rect1.shapeColor = "blue"
+  rect2.shapeColor  ="blue"
+}
+
+function draw() {
+  background(0,255,0);
+  collided();
+  rect1.x=mouseX 
+  rect1.y=mouseY
+  
+  drawSprites();
+}
+function collided(){
+  if((rect1.x-rect2.x)<=(rect1.width+rect2.width)/2&&(rect2.y-rect1.y)<=(rect1.height+rect2.height)/2&&(rect1.y-rect2.y)<=(rect1.height+rect2.height)/2&&(rect2.x-rect1.x)<=(rect1.width+rect2.width)/2){
+    rect1.shapeColor="pink"
+    rect2.shapeColor  ="pink"
+ }else{
+   rect1.shapeColor="blue"
+   rect2.shapeColor="blue"
+
+ }
+}
